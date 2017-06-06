8f31474f5cc9e4888a555f9d6a1ad1570917a7be
Sami Ahvenniemi <spahvenn@gmail.com>
2017-06-06 17:20:53 +0300

    Added test for getting user

--- test/index.js
+++ test/index.js
@@ -7,6 +7,23 @@ chai.should();
 
 request = request('http://localhost:3001')
 
+function getJwt() {
+  return request
+    .post('/login')
+    .send(
+      {
+        email: secret.admin.email,
+        password: secret.admin.password
+      }
+    )
+    .expect(200)
+}
+
+function getUserIdFromToken(token) {
+  var decoded = jwt.verify(token, secret.jwtSecret);
+  return decoded.user.id;
+}
+
 describe('GET /user', function() {
   it('deny access without token', function(done) {
     request.get('/user/1')
@@ -16,15 +33,7 @@ describe('GET /user', function() {
 
 describe('POST /login', function() {
   it('Ask for access token', function(done) {
-    request
-      .post('/login')
-      .send(
-        {
-          email: secret.admin.email,
-          password: secret.admin.password
-        }
-      )
-      .expect(200)
+      getJwt()
       .then(res => {
         jwt.verify(res.body.token, secret.jwtSecret, (err, decoded) => {
           const email = decoded.user.email
@@ -34,3 +43,35 @@ describe('POST /login', function() {
       })
   });
 });
+
+describe('User tests', function() {
+  it('GET user', function(done) {
+    getJwt()
+      .then(res => {
+        const userId = getUserIdFromToken(res.body.token)
+        request
+          .get('/user/'+userId)
+          .set('Authorization', 'Bearer '+res.body.token)
+          .expect(200)
+          .then(res => {
+            const email = res.body.user.email;
+            email.should.equal(secret.admin.email)
+            done()
+          })
+      })
+  })
+
+  it('GET non-existent user', function(done) {
+    getJwt()
+      .then(res => {
+        request
+          .get('/user/1')
+          .set('Authorization', 'Bearer '+res.body.token)
+          .expect(404)
+          .then(res => {
+            done()
+          })
+      })
+  })
+})
+
