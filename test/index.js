7657332c54e82c8005a07698fe26f06388e8940f
Sami Ahvenniemi <spahvenn@gmail.com>
2017-06-06 16:16:26 +0300

    Added auth middleware and some tests

--- test/index.js
+++ test/index.js
@@ -1,13 +1,36 @@
-import http from 'http';
-import assert from 'assert';
+let request = require('supertest')
+import secret from '../secret/secret'
+import jwt from 'jsonwebtoken'
+import chai from 'chai'
 
-import '../lib/index.js';
+chai.should();
 
-describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
-      assert.equal(200, res.statusCode);
-      done();
-    });
+request = request('http://localhost:3001')
+
+describe('GET /user', function() {
+  it('deny access without token', function(done) {
+    request.get('/user/1')
+      .expect(401, done);
+  });
+});
+
+describe('POST /login', function() {
+  it('Ask for access token', function(done) {
+    request
+      .post('/login')
+      .send(
+        {
+          email: secret.admin.email,
+          password: secret.admin.password
+        }
+      )
+      .expect(200)
+      .then(res => {
+        jwt.verify(res.body.token, secret.jwtSecret, (err, decoded) => {
+          const email = decoded.user.email
+          email.should.equal(secret.admin.email)
+          done();
+        })
+      })
   });
 });
